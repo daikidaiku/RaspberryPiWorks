6b98e746e5f405b76efa184ea446ec8c8f270ad0
daikidaiku <daiki_daiku@icloud.com>
2023-11-18 20:42:43 +0900

    update

--- touchsesor.py
+++ touchsesor.py
@@ -3,4 +3,14 @@ import time
 import os
 
 #sensor pin define
-touch = 26
+GPIO_PIN = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN,GPIO.IN)
+while True:
+    if(GPIO.input(GPIO_PIN)==GPIO.HIGH):
+        print("反応あり！")
+        time.sleep(1)
+    else:
+        print("反応なし")
+        time.sleep(1)
