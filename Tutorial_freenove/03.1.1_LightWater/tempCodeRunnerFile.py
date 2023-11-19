c4e248e2676877cfab727d326f38a7cf300c0934
daikidaiku <daiki_daiku@icloud.com>
2023-11-19 19:17:01 +0900

    231119 tutorial 9.11まで

---
+++ Tutorial_freenove/03.1.1_LightWater/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+GPIO.setup(ledPins, GPIO.OUT)   # set all ledPins to OUTPUT mode
+    GPIO.output(ledPins, GPIO.HIGH) # make all ledPins output HIGH level, turn off all led
--- touchsesor.py
+++ touchsesor.py
@@ -2,15 +2,29 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-#sensor pin define
-GPIO_PIN = 26
+GPIO_PIN = 26 #sensor pin define
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_PIN,GPIO.IN)
-while True:
-    if(GPIO.input(GPIO_PIN)==GPIO.HIGH):
-        print("反応あり！")
-        time.sleep(1)
-    else:
-        print("反応なし")
-        time.sleep(1)
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PIN,GPIO.IN)
+
+
+def loop():
+    while True:
+        if(GPIO.input(GPIO_PIN)==GPIO.HIGH):
+            print("反応あり！")
+            time.sleep(1)
+        else:
+            print("反応なし")
+            time.sleep(1)
+            
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == '__main__':    # Program entrance
+    print ('Program is starting ... \n')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:   # Press ctrl-c to end the program.
+        destroy()
